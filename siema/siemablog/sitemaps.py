b760f7323542c8fdd53b82d96bb190c9ab5ebcda
s-nix0 <styxnix06@gmail.com>
2023-11-19 20:28:07 +0800

    Update beta 0.3.2 - Update sitemaps

--- siema/siemablog/sitemaps.py
+++ siema/siemablog/sitemaps.py
@@ -5,12 +5,7 @@ from django.views.generic import TemplateView
 class SitemapView(TemplateView):
     template_name = 'sitemap.xml'
 
-    def get_context_data(self, **kwargs):
-        # Define your URLs here or retrieve them from your database
-        urls = [
-            {"loc": "/", "lastmod": "2023-01-01", "changefreq": "monthly", "priority": "1.0"},
-            {"loc": "/dashboard", "lastmod": "2023-01-02", "changefreq": "monthly", "priority": "0.8"},
-            {"loc": "/kontak", "lastmod": "2023-01-03", "changefreq": "monthly", "priority": "0.8"},
-            {"loc": "/akun/login", "lastmod": "2023-01-04", "changefreq": "monthly", "priority": "0.8"},
-        ]
-        return {'urls': urls}
+    def render_to_response(self, context, **response_kwargs):
+        response = super().render_to_response(context, **response_kwargs)
+        response['Content-Type'] = 'application/xml'
+        return response
