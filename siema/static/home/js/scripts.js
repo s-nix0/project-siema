08ffa3f6a6200b5d706ceded602eeb03437dcec6
s-nix0 <styxnix06@gmail.com>
2024-01-04 21:09:48 +0800

    Update beta 0.6.1 - Major update add documentation page

--- siema/static/home/js/scripts.js
+++ siema/static/home/js/scripts.js
@@ -24,7 +24,7 @@ const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
 			entry.target.classList.add("show");
-		} 
+		}
 		// else {
 		// 	entry.target.classList.remove("show");
 		// }
@@ -33,3 +33,33 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const imageWrapper = document.querySelector(".image-wrapper");
+const imageItems = document.querySelectorAll(".image-wrapper > *");
+const imageLength = imageItems.length;
+const perView = 3;
+let totalScroll = 0;
+const delay = 2000;
+
+imageWrapper.style.setProperty("--per-view", perView);
+for (let i = 0; i < perView; i++) {
+	imageWrapper.insertAdjacentHTML("beforeend", imageItems[i].outerHTML);
+}
+
+let autoScroll = setInterval(scrolling, delay);
+
+function scrolling() {
+	totalScroll++;
+	if (totalScroll == imageLength + 1) {
+		clearInterval(autoScroll);
+		totalScroll = 1;
+		imageWrapper.style.transition = "0s";
+		imageWrapper.style.left = "0";
+		autoScroll = setInterval(scrolling, delay);
+	}
+	const widthEl =
+		document.querySelector(".image-wrapper > :first-child").offsetWidth + 24;
+	imageWrapper.style.left = `-${totalScroll * widthEl}px`;
+	imageWrapper.style.transition = ".3s";
+}
